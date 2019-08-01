1ec5dd10e4b0bd742eb433d78b2dc5786ea449ee
christian-bromann <mail@christian-bromann.com>
2019-08-01 14:07:14 +0200

    clean up common modules

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,7 @@
 import Settings from './settings'
 
 export default class Common {
-  public static moduleSetting (settingName: string): Settings {
-    return Settings.moduleSetting(settingName);
-  }
+    public static moduleSetting(settingName: string): Settings {
+        return Settings.moduleSetting(settingName)
+    }
 }
--- src/common/settings.ts
+++ src/common/settings.ts
@@ -1,19 +1,19 @@
 export default class Settings {
-  private static _moduleSettings: Map<string, Settings>  = new Map()
+    private static _moduleSettings: Map<string, Settings> = new Map()
 
-  /**
-   * @param {string} settingName
-   * @return {!Common.Setting}
-   */
-  public static moduleSetting(settingName: string): Settings {
-    const setting = this._moduleSettings.get(settingName);
-    if (!setting)
-      throw new Error('No setting registered: ' + settingName);
-    return setting;
-  }
+    /**
+     * @param {string} settingName
+     * @return {!Common.Setting}
+     */
+    public static moduleSetting(settingName: string): Settings {
+        const setting = this._moduleSettings.get(settingName)
+        if (!setting) {
+            throw new Error('No setting registered: ' + settingName)
+        }
+        return setting
+    }
 
-  public get() {
-    return true
-  }
+    public get (): boolean {
+        return true
+    }
 }
-
