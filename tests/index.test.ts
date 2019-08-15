5196dbdb0b4559e8823c2fb9fba937818caf02e7
farhan-sauce <39295560+farhan-sauce@users.noreply.github.com>
2019-08-15 18:55:14 +0530

    fix data mutation issue (#12)
    * fix data mutation issue
    
    * update unit tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,61 +1,71 @@
 import Tracelib from '../src/index'
 import JANK_TRACE_LOG from './__fixtures__/jankTraceLog.json'
 
+let trace: Tracelib
+beforeAll(() => {
+    trace = new Tracelib(JANK_TRACE_LOG)
+})
+
 test('should contain traceLog', () => {
     const sampleTrace = new Tracelib({ foo: 'bar' })
     expect(sampleTrace.tracelog).toEqual({ foo: 'bar' })
 })
 
 test('should get FPS', () => {
-    const trace = new Tracelib(JANK_TRACE_LOG)
     const result = trace.getFPS()
     expect(result).toMatchSnapshot()
 })
 
-test('getSummary: should get summary data', () => {
-    const trace = new Tracelib(JANK_TRACE_LOG)
-    const result = trace.getSummary()
-    expect(result).toMatchSnapshot()
+describe('getSummary', () => {
+    it('should get summary data', () => {
+        const result = trace.getSummary()
+        expect(result).toMatchSnapshot()
+    })
+
+    it('should not throw error on second call of getSummary', () => {
+        const result = trace.getSummary()
+        expect(result).toMatchSnapshot()
+    })
+
+    it('should throw error if main track is missing', () => {
+        const tracelib = new Tracelib([])
+        expect(() => tracelib.getSummary())
+            .toThrow(new Error('MainTrack is missing in traceLog'))
+    })
+
+    it('should get summary data between passed range', () => {
+        const result = trace.getSummary(289960055.634, 289960729.717)
+        expect(result).toMatchSnapshot()
+    })
 })
 
-test('getSummary: should throw error if main track is missing', () => {
-    const trace = new Tracelib([])
-    expect(() => trace.getSummary())
-        .toThrow(new Error('MainTrack is missing in traceLog'))
-})
+describe('getWarningCounts', () => {
+    it('should get warning counts', () => {
+        const result = trace.getWarningCounts()
+        expect(result).toMatchSnapshot()
+    })
 
-test('should get summary data between passed range', () => {
-    const trace = new Tracelib(JANK_TRACE_LOG)
-    const result = trace.getSummary(289960055.634, 289960729.717)
-    expect(result).toMatchSnapshot()
-})
-
-test('getWarningCounts: should get warning counts', () => {
-    const trace = new Tracelib(JANK_TRACE_LOG)
-    const result = trace.getWarningCounts()
-    expect(result).toMatchSnapshot()
-})
-
-test('getWarningCounts: should throw error if main track is missing', () => {
-    const trace = new Tracelib([])
-    expect(() => trace.getWarningCounts())
-        .toThrow(new Error('MainTrack is missing in traceLog'))
+    it('should throw error if main track is missing', () => {
+        const tracelib = new Tracelib([])
+        expect(() => tracelib.getWarningCounts())
+            .toThrow(new Error('MainTrack is missing in traceLog'))
+    })
 })
 
 test('should get memory counters', () => {
-    const trace = new Tracelib(JANK_TRACE_LOG)
     const result = trace.getMemoryCounters()
     expect(result).toMatchSnapshot()
 })
 
-test('mainTrackEvents: should get events', () => {
-    const trace = new Tracelib(JANK_TRACE_LOG)
-    const result = trace.getMainTrackEvents()
-    expect(result.length).toEqual(56244)
-})
+describe('mainTrackEvents', () => {
+    it('should get events', () => {
+        const result = trace.getMainTrackEvents()
+        expect(result.length).toEqual(56244)
+    })
 
-test('mainTrackEvents: should throws error if main track is missing', () => {
-    const trace = new Tracelib([])
-    expect(() => trace.getMainTrackEvents())
-        .toThrow(new Error('MainTrack is missing in traceLog'))
+    it('should throws error if main track is missing', () => {
+        const tracelib = new Tracelib([])
+        expect(() => tracelib.getMainTrackEvents())
+            .toThrow(new Error('MainTrack is missing in traceLog'))
+    })
 })
